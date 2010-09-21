ce61f9c55dafeba0987928feda73151042e8111d
Miika Leppänen <maleppanen@gmail.com>
2010-09-21 21:45:47 +0300

    add validations and root method

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,9 @@
 class Page < ActiveRecord::Base
+  validates_uniqueness_of :short_name
+  validates_presence_of :title, :short_name, :order
+
+  def self.root
+    find(:first, :order => '`order` ASC')
+  end
+    
 end
